d087f0be115e18ab0d6f516ba62f7248a30adfb6
lericson <ludvig@lericson.se>
2009-04-05 03:22:18 +0200

    Refactor name blah.Blah to irken.Connection.

--- blah.py
+++ blah.py
@@ -1,16 +1,8 @@
 import logging
-from irken.base import Connection
-from irken.dispatch import CommonDispatchMixin
-from irken.encoding import EncodingMixin
-from irken.io import SimpleSocketMixin
-from irken.utils import AutoRegisterMixin
+from irken import Connection
 
 logging.basicConfig(level=logging.DEBUG)
 
-class Blah(AutoRegisterMixin, EncodingMixin, SimpleSocketMixin,
-           CommonDispatchMixin, Connection): pass
-
-b = Blah(nick="toxik-bot")
-b.autoregister = "toxik-bot-u", "Toxik Bot R"
+b = Connection(nick="toxik-bot", autoregister=("toxik-bot-u", "Toxik Bot R"))
 b.connect(("irc.lericson.se", 6667))
 b.run_forever()
--- irken/__init__.py
+++ irken/__init__.py
@@ -1,2 +1,11 @@
 class IRCError(Exception): pass
 class UnhandledCommandError(IRCError): pass
+
+from irken.base import Connection
+from irken.dispatch import CommonDispatchMixin
+from irken.encoding import EncodingMixin
+from irken.io import SimpleSocketMixin
+from irken.utils import AutoRegisterMixin
+
+class Connection(AutoRegisterMixin, EncodingMixin, SimpleSocketMixin,
+                 CommonDispatchMixin, Connection): pass
