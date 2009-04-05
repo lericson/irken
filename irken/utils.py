0a2a6c8f3d28fc552c89cbdd7dc6b66f4a8444bf
lericson <ludvig@lericson.se>
2009-04-05 04:04:35 +0200

    Add debug mixin.

--- irken/utils.py
+++ irken/utils.py
@@ -10,3 +10,13 @@ class AutoRegisterMixin(object):
         self.send_cmd(None, "USER", (user, "*", "*", real))
         self.send_cmd(None, "NICK", (self.nick,))
         return rv
+
+from functools import partial
+
+class DebugMixin(object):
+    def _do_cmd(name, prefix, command, args):
+        parts = (name, prefix or "", command, args or [])
+        print "%s: prefix=%r, command=%s, args=%r" % parts
+    recv_cmd = staticmethod(partial(_do_cmd, "recv"))
+    send_cmd = staticmethod(partial(_do_cmd, "send"))
+    del _do_cmd
