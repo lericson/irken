969febc91d4b7347d34ed0fed6d6f8033a9c3b08
lericson <ludvig@lericson.se>
2009-04-05 04:16:05 +0200

    Move blah.py to example.py

--- blah.py
+++
@@ -1,8 +0,0 @@
-import logging
-from irken import Connection
-
-logging.basicConfig(level=logging.DEBUG)
-
-b = Connection(nick="toxik-bot", autoregister=("toxik-bot-u", "Toxik Bot R"))
-b.connect(("irc.lericson.se", 6667))
-b.run_forever()
--- example.py
+++ example.py
@@ -1,28 +1,13 @@
-class MyIRCBot(irken.Connection):
-    def on_joined(self, who, where):
-        if who is self:
-            print "I joined", where
-        else:
-            print who, "joined", where
+import logging
+import irken
 
-    def on_message(self, sender, receiver, text):
-        if receiver is self:
-            logger.debug("Private message: %s", text)
-        print "<%s:%s> %s" % (sender.nickname, receiver, text)
+NICKNAME = "irken-nick"
+USERNAME = "irken-user"
+REALNAME = "irken realname"
+ADDRNAME = "irc.lericson.se", 6667
 
-# Method 1: plain setting the socket attr.
-b = MyIRCBot(nick="toxik-bot", autoregister=("toxik-bot-u", "Toxik Bot R"))
-b.socket = a_socket
-b.run_forever()
+logging.basicConfig(level=logging.DEBUG)
 
-# Method 2: creating with a utility method.
-b = MyIRCBot.make_connected(address=("irc.lericson.se", 6667), nick="toxik-bot",
-                            autoregister=("toxik-bot-u", "Toxik Bot R"))
-b.run_forever()
-
-# Method 3: using a connector.
-def spawner(connector):
-    print "Connection attempt #%d" % (connector.attempts + 1,)
-    return MyIRCBot(nick="toxik-bot", autoregister=("toxik-bot-u", "Toxik Bot R"))
-conntor = Connector(spawner=spawner, address=("irc.lericson.se", 6667))
-conntor.run_forever()
+bot = irken.Connection(nick=NICKNAME, autoregister=(USERNAME, REALNAME))
+bot.connect(ADDRNAME)
+bot.run_forever()
