51319b704a66b31ba89b51aa94355cf5617574e4
lericson <ludvig@lericson.se>
2009-04-05 03:22:49 +0200

    Add initializement argument for autoregistration.

--- irken/utils.py
+++ irken/utils.py
@@ -1,4 +1,9 @@
 class AutoRegisterMixin(object):
+    def __init__(self, *args, **kwds):
+        if "autoregister" in kwds:
+            self.autoregister = kwds.pop("autoregister")
+        super(AutoRegisterMixin, self).__init__(*args, **kwds)
+
     def connect(self, *args, **kwds):
         rv = super(AutoRegisterMixin, self).connect(*args, **kwds)
         user, real = self.autoregister
